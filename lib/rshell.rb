5c3f3e74fac10808eb892724e4e21d0979bac692
Andrea Salicetti <andrea.salicetti@gmail.com>
2011-05-12 14:04:15 +0200

    Some patches.

--- lib/rshell.rb
+++ lib/rshell.rb
@@ -12,13 +12,3 @@ module Rshell
   end
 end
 
-class String
-  def bold
-    "\033[1;39m#{self}\033[0m"
-  end
-
-  def colorize(color_code)
-    return color_code ? "#{color_code}#{self}\033[0m" : self
-  end
-end
-
